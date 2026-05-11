871104f03e528007d7ec815dcb7c7d45ef628100
black-magic-code <lkashyp6@gmail.com>
2026-05-11 14:03:58 +0530

    Update pouchdb.js

--- src/database/pouchdb.js
+++ src/database/pouchdb.js
@@ -1,25 +1,33 @@
 "use client"
 
-import PouchDB from "pouchdb-browser"
+let dbInstance = null
 
-let db = null
-
-export function getDB() {
+export async function getDB() {
 
   if (typeof window === "undefined") {
     return null
   }
 
-  if (!db) {
-
-    db = new PouchDB(
-      process.env.NEXT_PUBLIC_DB_NAME || "inventory",
-      {
-        auto_compaction: true,
-        revs_limit: 20
-      }
-    )
+  if (dbInstance) {
+    return dbInstance
   }
 
-  return db
+  const PouchDB =
+    (await import("pouchdb-browser"))
+      .default
+
+  dbInstance = new PouchDB(
+
+    process.env.NEXT_PUBLIC_DB_NAME ||
+    "inventory",
+
+    {
+
+      auto_compaction: true,
+
+      revs_limit: 20
+    }
+  )
+
+  return dbInstance
 }
