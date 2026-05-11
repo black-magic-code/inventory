d354ee3e5ece44a4d4b46690648d64762a0bc9f8
black-magic-code <lkashyp6@gmail.com>
2026-05-11 13:05:09 +0530

    Update page.js

--- src/app/offline/page.js
+++ src/app/offline/page.js
@@ -1,7 +1,39 @@
 export default function OfflinePage() {
+
   return (
-    <div>
-      No internet connection
+
+    <div
+      className="
+      min-h-screen
+      flex
+      items-center
+      justify-center
+      p-6
+      text-center
+    "
+    >
+
+      <div className="space-y-4">
+
+        <h1
+          className="
+          text-3xl
+          font-bold
+        "
+        >
+          You Are Offline
+        </h1>
+
+        <p
+          className="
+          text-gray-500
+        "
+        >
+          Please check your internet connection.
+        </p>
+
+      </div>
+
     </div>
   )
 }
